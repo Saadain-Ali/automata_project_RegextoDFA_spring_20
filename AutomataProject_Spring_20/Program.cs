1b95a89299264a777410671ad5a5d8346f107a91
Saadi <ali.saadain@gmail.com>
2020-05-05 22:17:00 +0500

    Final changes visual transition table is added

--- AutomataProject_Spring_20/Program.cs
+++ AutomataProject_Spring_20/Program.cs
@@ -10,46 +10,53 @@ namespace AutomataProject_Spring_20
     {
         static void Main(string[] args)
         {
+
+            //our reg expression = ba(a+b)abaa+ba(a+b)*
+
+            //////////////////////// TRANSITION TABLE /////////////////////////////////////////
             Dictionary<int, Dictionary<char, int>> dfa = new Dictionary<int, Dictionary<char, int>>()
             {
                 {
-                    0,new Dictionary<char, int>{{ 'a',3 } , {'b',1} }
+                  //Current State           //next_state_for 'a'       //next_state_for 'b'
+                     0,new Dictionary<char, int>{  { 'a',3 } ,               {'b',1} }
                 },
                 {
-                     1,new Dictionary<char, int>{{ 'a',2 } , {'b',3} }
+                     1,new Dictionary<char, int>{{ 'a',2 } ,                {'b',3} }
                 },
                 {
-                     2,new Dictionary<char, int>{{ 'a',2 } , {'b',2} } //Final state
+                     2,new Dictionary<char, int>{{ 'a',2 } ,                {'b',2} }
                 },
                 {
-                     3,new Dictionary<char, int>{{ 'a',3 } , {'b',3} }
+                     3,new Dictionary<char, int>{{ 'a',3 } ,                {'b',3} }
                 }
             };
 
-            int[] finalState = {2};            
-            bool run = true;
-            while (run)
+            int[] finalState = {2};    // Final States our DFA         
+
+           
+            while (true)
             {
                 string input = "";
-                Console.WriteLine("Press Esc to quit , r to auto generate string , or press s");
-                char p = Convert.ToChar(Console.ReadLine());
-                switch (p)
+                Console.WriteLine("Press Esc to quit , r to auto generate string , or press enter to enter your string");
+                ConsoleKeyInfo c = Console.ReadKey(true);
+
+                if (c.Key == ConsoleKey.Escape)
+                    break;  
+
+                else if (c.Key == ConsoleKey.R)
                 {
-                    case 'q':
-                        run = false;
-                        break;
-                    case 'r':
-                        input = random(8);
-                        Console.WriteLine(input);
-                        break;
-                    default:
-                        Console.WriteLine("Enter your string");
-                        input = Console.ReadLine();
-                        break;
+                    input = random();
+                    Console.WriteLine(input);
+                }
+
+                else
+                {
+                    Console.Write("Enter your string : ");
+                    input = Console.ReadLine();
                 }
                 bool ans = accepts(dfa, 0, finalState, input);
-                Console.WriteLine("Given string is " + ans);
-                Console.WriteLine();
+                Console.WriteLine("\t\t\nGiven string is " + ans + "\n\n");
+                
             }
            
         }
@@ -57,14 +64,28 @@ namespace AutomataProject_Spring_20
         {
             int state = initial;
             bool final;
+            Console.WriteLine("current_state\tcharacter\tnext_state");
             foreach (char c in s)
+            {
+                if (c != 'a' && c != 'b')
+                {
+                    Console.WriteLine($"{c} is not a part of language {{a,b}}");
+                    return false;
+                }
+                Console.Write(state+"\t\t"); //to print the transitions
                 state = transitions[state][c];
+                Console.WriteLine( c + "\t\t"+ state );
+                
+            }
+               
             return final  = true ? arr.Contains(state) : false;
         }
-        public static string random(int length)
+        public static string random()
         {
+            
            string s = "";
             Random r = new Random(DateTime.Now.Millisecond);
+            int length = r.Next(3,11);
             for (int i = 0; i < length; i++)
             {
                 int w = r.Next(97,99);
